387eb66f2c8bd9f458ce054c5fb8c1e0e910bd15
温日光 <@192.168.0.107>
2019-07-24 22:43:48 +0800

    just test.

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -1,9 +1,23 @@
 package helloword;
 
+import java.util.Random;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Random ra = null;
+		
+		while(true) {
+			int flaaag = ra.nextInt(1);
+			
+			if(flaaag == 1) {
+				System.out.println("…½ÊºÀ²Äã¡£");
+			}else {
+				System.out.println("I'am sorry , Brother FeiFan.");
+			}
+			
+		}
+
 	}
 
 }
